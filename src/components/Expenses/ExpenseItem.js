4b647e909360a4741433e625d4297f7714bf83ec
Aaditya Ghimire <ghimireaditya@gmail.com>
2022-06-22 18:29:42 +0545

    Use of state in react

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,24 @@
 import "./ExpenseItem.css";
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
-
 function ExpenseItem(props) {
+  //let title = props.title;
+  const [titles, setTitles] = useState(props.title);
+  const clickHandler = () => {
+    setTitles("Updated");
+    console.log(titles);
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{titles}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }

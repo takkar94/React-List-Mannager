01a94fc350b78129016305e07dc574875d8c2bea
Aaditya Ghimire <ghimireaditya@gmail.com>
2022-06-27 13:35:02 +0545

    Optimized the conditional statements

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -8,13 +8,31 @@ import './Expenses.css';
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState('2020');
 
+  //responsible for changing the the filtered data visually  
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
+  //responsible for changing the contents of the list
   const filteredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString() === filteredYear;
   })
+
+  //optimizing the jsx conditional statements
+  let expenseContent = <p>No Expense Found</p>
+
+
+  if(filteredExpenses.length >0 ) {
+    expenseContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
+
 
   return (
     <div>
@@ -23,18 +41,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.length === 0 && 
-            filteredExpenses.map((expense) => (
-              <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-              />
-            ))
-
-        }
-        {filteredExpenses.length === 0 && <p>No Expenses Found</p> }
+        {expenseContent} {/* Points to the expenseContent variable */}
       </Card>
     </div>
   );
